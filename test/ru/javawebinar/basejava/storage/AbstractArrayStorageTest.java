887c1cf9dee01aacb2b86c66401509bc793ad9dc
Ashungren <Ashungren@mail.ru>
2017-12-16 18:11:07 +0300

    preparation to HW5

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -14,17 +14,22 @@ import static org.junit.Assert.assertTrue;
 public abstract class AbstractArrayStorageTest {
     private Storage storage;
 
-    private static final String UUID_1="uuid1";
-    private static final Resume RESUME_1 = new Resume(UUID_1);
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+    private static final String UUID_4 = "uuid4";
 
-    private static final String UUID_2="uuid2";
-    private static final Resume RESUME_2 = new Resume(UUID_2);
+    private static final Resume RESUME_1;
+    private static final Resume RESUME_2;
+    private static final Resume RESUME_3;
+    private static final Resume RESUME_4;
 
-    private static final String UUID_3="uuid3";
-    private static final Resume RESUME_3 = new Resume(UUID_3);
-
-    private static final String UUID_4="uuid4";
-    private static final Resume RESUME_4 = new Resume(UUID_4);
+    static {
+        RESUME_1 = new Resume(UUID_1);
+        RESUME_2 = new Resume(UUID_2);
+        RESUME_3 = new Resume(UUID_3);
+        RESUME_4 = new Resume(UUID_4);
+    }
 
     protected AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
@@ -107,10 +112,10 @@ public abstract class AbstractArrayStorageTest {
     @Test
     public void getAll() throws Exception {
         Resume[] array = storage.getAll();
-        assertEquals(3,array.length);
-        assertEquals(RESUME_1,array[0]);
-        assertEquals(RESUME_2,array[1]);
-        assertEquals(RESUME_3,array[2]);
+        assertEquals(3, array.length);
+        assertEquals(RESUME_1, array[0]);
+        assertEquals(RESUME_2, array[1]);
+        assertEquals(RESUME_3, array[2]);
     }
 
     @Test
@@ -119,10 +124,10 @@ public abstract class AbstractArrayStorageTest {
     }
 
     private void assertGet(Resume r) {
-    assertEquals(r,storage.get(r.getUuid()));
+        assertEquals(r, storage.get(r.getUuid()));
     }
 
     private void assertSize(int size) {
-        assertEquals(size,storage.size());
+        assertEquals(size, storage.size());
     }
 }
