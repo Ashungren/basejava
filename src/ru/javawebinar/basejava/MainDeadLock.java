2c32f0157cc38e9f708fad1461f43e8226a44d2c
Ashungren <Ashungren@mail.ru>
2018-01-31 18:18:00 +0300

    HW11 ed1

--- src/ru/javawebinar/basejava/MainDeadLock.java
+++ src/ru/javawebinar/basejava/MainDeadLock.java
@@ -1,41 +1,30 @@
 package ru.javawebinar.basejava;
 
 public class MainDeadLock {
-    private static final Object keyA = new Object();
-    private static final Object keyB = new Object();
+    private static final String keyA = "key A";
+    private static final String keyB = "key B";
 
     public static void main(String[] args) {
         thread0.start();
         thread1.start();
     }
 
-    private static Thread thread0 = new Thread(() -> {
-        synchronized (keyA) {
-            System.out.println(Thread.currentThread() + " got key A");
-            try {
-                Thread.sleep(500);
-                System.out.println(Thread.currentThread() + " trying to get key B");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            synchronized (keyB) {
-                System.out.println(Thread.currentThread() + " got key B");
-            }
-        }
-    });
+    private static Thread thread0 = new Thread(() -> lock(keyA, keyB));
 
-    private static Thread thread1 = new Thread(() -> {
-        synchronized (keyB) {
-            System.out.println(Thread.currentThread() + " got key B");
+    private static Thread thread1 = new Thread(() -> lock(keyB, keyA));
+
+    private static void lock(String a, String b) {
+        synchronized (a) {
+            System.out.println(Thread.currentThread() + " got " + a);
             try {
                 Thread.sleep(500);
-                System.out.println(Thread.currentThread() + " trying to get key A");
+                System.out.println(Thread.currentThread() + " trying to get " + b);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            synchronized (keyA) {
-                System.out.println(Thread.currentThread() + " got key A");
+            synchronized (b) {
+                System.out.println(Thread.currentThread() + " got " + b);
             }
         }
-    });
+    }
 }
